fd12f9b8f7f4e3615927b9c204813c6745dd15e7
hisreal123 <omosgoerge@gmail.com>
2024-11-05 12:42:41 +0100

    1-main.py

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -1,12 +1,16 @@
-from flask import Flask, jsonify, render_template
+#!/usr/bin/env python3
+""" A simple flask app"""
+
+
+from flask import Flask, render_template
 from flask_cors import CORS
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
-
 @app.route('/')
 def index():
+  """ a single route"""
   return render_template('0-index.html')
 
 
