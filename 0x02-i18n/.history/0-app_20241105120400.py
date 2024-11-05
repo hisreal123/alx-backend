322c9f37bbc13b63b53141c8e18f64db86e413f3
hisreal123 <omosgoerge@gmail.com>
2024-11-05 12:38:44 +0100

    1-main.py

--- 0x02-i18n/.history/0-app_20241105120400.py
+++ 0x02-i18n/.history/0-app_20241105120400.py
@@ -1,6 +1,7 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
 
+
 app = Flask(__name__, static_folder='static', template_folder='templates')
 CORS(app)
 
--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,9 +1,14 @@
 from flask import Flask, jsonify, render_template
 from flask_cors import CORS
+from flask_babel import Babel
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
+# app.config.from_pyfile('mysettings.cfg')
+babel = Babel(app)
 CORS(app)
 
+class Config:
+    LANGUAGES = ['en', 'us']
 
 @app.route('/')
 def index():
